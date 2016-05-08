8a05b0aa263cd5af507f816e4529ff7cd6c2b8fa
Taylor Hurt <taylor.a.hurt@gmail.com>
2016-05-08 07:12:09 +0000

    simplify Object#entries, simplify name filtering steps, make code more fp oriented

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // fetchStr :: Object -> Promise String
 function fetchStr({ url, init }) {
   return window.fetch(url, init).then((res, rej) => {
@@ -16,22 +15,14 @@ function toHTMLDocument(pstr) {
   })
 }
 
-// returns an iterator for an Object. It has this data structure: [key, value]
-Object.prototype.entries = function() {
-  let keys = Reflect.ownKeys(this)[Symbol.iterator]()
-  return {
-    [Symbol.iterator]() {
-      return this
-    },
-    next: () => {
-       let { done, value: key } = keys.next()
-       if (!done) {
-         return { value: [key, this[key]] }
-       }
-       return { done }
-    }
-  }
+// pipe :: ((a, b, ... -> e), (e -> f), ..., (y -> z)) -> (a, b, ...) -> z
+function pipe(...fns) {
+  return (...x) =>
+    fns.slice(1).reduce((x, fn) => fn(x), fns[0](...x))
 }
+
+// fetchDOM :: Object -> Promise HTMLDocument
+var fetchDOM = pipe(fetchStr, toHTMLDocument)
 
 // gets all html elements with data-key attribute and stores them in a dictonary.
 // later, using DataBind function triggers a setter function for that key
@@ -51,53 +42,73 @@ var DataBind = (() => {
 
   // DataBind function
   return (kv_pairs) => {
-    for (var [k, v] of kv_pairs.entries()) {
+    for (var [k, v] of entries(kv_pairs)) {
       _binder[k] = v
     }
   }
 })()
 
-//
-var gh_headers = new window.Headers()
-gh_headers.append('Accept', 'application/vnd.github.v3.html')
-//
+// entries :: Object -> Array
+function entries(obj) {
+  return Reflect.ownKeys(obj).map(key => [key, obj[key]])
+}
 
-Promise.all([
-  toHTMLDocument(fetchStr({
-    url: 'http://ramdajs.com/0.21.0/docs/'
-  })),
-  toHTMLDocument(fetchStr({
-    url: 'http://api.github.com/repos/sanctuary-js/sanctuary/readme',
-    init: { 'headers': gh_headers }
-  }))
-])
-.then(([$ramda, $sanctuary]) => {
-  var x_fns = Array.from($ramda.querySelectorAll('section.card'))
-    .map(card => card.getAttribute('id'))
-    .sort()
+// accept :: String -> Headers { 'Accept': String }
+function accept(mediaTypeStr) {
+  var accept_hdr = new window.Headers()
+  accept_hdr.append('Accept', mediaTypeStr)
+  return accept_hdr
+}
 
-  var y_fns = Array.from($sanctuary.querySelectorAll('h4[name]'))
-    .map(h4 => h4.getAttribute('name').split('-').reverse()[0])
-    .sort()
+///////////////////////////////////////////////////
 
-  var shared_fns = x_fns
-    .map(x_fn => [x_fn, y_fns.includes(x_fn)])
-    .filter(tuple => tuple[1])
-    .map(tuple => tuple[0])
+// input data
+var ramda = {
+  url: 'http://ramdajs.com/0.21.0/docs/'
+}
+var sanctuary = {
+  url: 'http://api.github.com/repos/sanctuary-js/sanctuary/readme',
+  init: { 'headers': accept('application/vnd.github.v3.html') }
+}
 
-  var x_only_fns = x_fns.filter(x_fn => !shared_fns.includes(x_fn))
-  var y_only_fns = y_fns.filter(y_fn => !shared_fns.includes(y_fn))
+Promise.all([ramda, sanctuary].map(fetchDOM))
+  // Perform DOM scraping to get function names
+  .then(([$R, $S]) => [
+    Array.from($R.querySelectorAll('section.card'))
+      .map(card => card.getAttribute('id'))
+      .sort()
+    ,
+    Array.from($S.querySelectorAll('h4[name]'))
+      .map(h4 => h4.getAttribute('name').split('-').reverse()[0])
+      .sort()
+  ])
 
-  DataBind({
-    "ramda-total": x_fns.length,
-    "sanctuary-total": y_fns.length,
-    "shared-count": shared_fns.length,
-    "shared-fns": shared_fns.join('\n'),
-    "ramda-only-count": x_fns.length - shared_fns.length,
-    "ramda-only-fns": x_only_fns.join('\n'),
-    "sanctuary-only-count": y_fns.length - shared_fns.length,
-    "sanctuary-only-fns": y_only_fns.join('\n')
+  // Determine shared function names, R-only names, and S-only names
+  .then(([R_names, S_names]) => [
+    R_names
+      .filter(n => S_names.includes(n))
+    ,
+    R_names
+      .filter(n => !S_names.includes(n))
+    ,
+    S_names
+      .filter(n => !R_names.includes(n))
+  ])
+
+  // Side-Effect: Bind data values to the page
+  .then(([shared, R_only, S_only]) => {
+    DataBind({
+      "ramda-total": R_only.length + shared.length,
+      "sanctuary-total": S_only.length + shared.length,
+      "shared-count": shared.length,
+      "shared-fns": shared.join('\n'),
+      "ramda-only-count": R_only.length,
+      "ramda-only-fns": R_only.join('\n'),
+      "sanctuary-only-count": S_only.length,
+      "sanctuary-only-fns": S_only.join('\n')
+    })
   })
-}, err => {
+
+.catch(err => {
   console.error(err)
 })
